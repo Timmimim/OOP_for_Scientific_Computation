1639b09d78984d73b52430fd42243b9bf25f7480
Timm Kühnel <t_kueh06@uni-muenster.de>
2020-05-25 17:52:30 +0200

    Add File-Output and data/ directory

--- blatt04/main.cc
+++ blatt04/main.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <algorithm>
 
 #include "src/ODE.h"
@@ -13,7 +14,16 @@ template class ODESolver<3ul>;
 template class ExplicitEuler<3ul>; 
 template class ImplicitEuler<3ul>; 
 
-
+std::ostream& operator<< (std::ostream& os, std::array<double,4> arr)
+{
+    for (int i = 0; i < arr.size(); ++i) { 
+        os << arr[i]; 
+        if (i != arr.size() - 1) 
+            os << ", "; 
+    } 
+    os << "\n";
+    return os; 
+}
 
 int main()
 {
@@ -21,6 +31,7 @@ int main()
     SIR<3> sir_exp(.4,.04,{977,3,0});
     SIR<3> sir_imp(.4,.04,{977,3,0});
     std::cout << "SIR works" << std::endl;
+
     ExplicitEuler<3> explicit_euler(sir_exp, 0, 1, 100);
     std::cout << "Explicit Constructor works" << std::endl;
     ImplicitEuler<3> implicit_euler(sir_imp, 0, 1, 100);
@@ -28,8 +39,28 @@ int main()
 
     auto explicit_solution = explicit_euler.solve();
     std::cout << "Explicit Solver works" << std::endl;
+    
+    std::cout << "Writing Explicit Euler result to file" << std::endl;
+    std::ofstream fs;
+    fs.open("blatt04/data/sir_explicit_euler.csv");
+    for (auto &arr : explicit_solution)
+    {
+        fs << arr;
+    }
+    fs.close();
+    std::cout << "File holding Explicit Euler data closed" << std::endl;
+
     auto implicit_solution = implicit_euler.solve();
     std::cout << "Implicit Solver works" << std::endl;
 
-    
+    std::cout << "Writing Implicit Euler result to file" << std::endl;
+    fs.open("blatt04/data/sir_implicit_euler.csv");
+    for (auto &arr : implicit_solution)
+    {
+        fs << arr;
+    }
+    fs.close();
+    std::cout << "File holding Implicit Euler data closed" << std::endl;
+
+    std::cout << "All done" << std::endl;
 }
