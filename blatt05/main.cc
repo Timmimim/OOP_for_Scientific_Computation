a1a316745ef3c766aa72a3152153c6b9d572bd13
Timm Kühnel <t_kueh06@uni-muenster.de>
2020-05-30 22:55:31 +0200

    Add Operator+ and Operator- plus Main with first Tests
    Op+ and Op- differentiate between (pure) integral and (mixed or pure) floating point input, and return int or double typed Polynomials, respectively.

---
+++ blatt05/main.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+
+#include "src/polynomial.h"
+
+
+int main()
+{
+    std::array<int,3> data_A {{1,2,3}};
+    std::array<int,4> data_B {{4,3,2,1}};
+    std::array<double,3> data_X {{1.,2.,3.}};
+    std::array<double,3> data_Z {{1.,3.,2.}};
+    std::array<double,4> data_Y {{4.,3.,2.,1.}};
+
+    Polynomial<int,3> A(data_A);
+    Polynomial<int,4> B(data_B);
+    Polynomial<double,3> X(data_X);
+    Polynomial<double,3> Z(data_Z);
+    Polynomial<double,4> Y(data_Y);
+
+    auto C = A + B;
+    std::cout << C << std::endl;
+
+    auto D = A - B;
+    std::cout << D << std::endl;
+    
+    auto E = X + Y;
+    std::cout << E << std::endl;
+    
+    auto F = X - Y;
+    std::cout << F << std::endl;
+
+    auto G = Y - X;
+    std::cout << G << std::endl;
+
+    auto H = X - Z;
+    std::cout << H << std::endl;
+}
--- blatt05/src/polynomial.h
+++ blatt05/src/polynomial.h
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <array>
 #include <type_traits>
+#include <cassert>
 
 template<typename T, size_t N, typename = typename std::enable_if<std::is_arithmetic<T>::value, T>::type>
 class Polynomial
@@ -14,18 +15,17 @@ public:
      */
     //========================
 
-    double operator() (const unsigned int exponent) const
+    T& operator() (unsigned int exponent)
     {
         assert(exponent < _coefficients.size());
         return _coefficients[exponent];
     }
 
-    double& operator() (const unsigned int exponent)
+    const T& operator() (unsigned int exponent) const
     {
         assert(exponent < _coefficients.size());
         return _coefficients[exponent];
     }
-
 
     Polynomial operator*= (const T& alpha)
     {
@@ -36,6 +36,116 @@ public:
         return *this;
     }
 
+    template<typename TYPE, size_t SIZE>
+    friend std::ostream& operator<<( std::ostream&, const Polynomial<TYPE,SIZE>& );
+
 private:
     std::array<T,N> _coefficients;
 };
+
+
+/*
+=====================================================================================
+===================== Free Operators for Class Polynomial ===========================
+=====================================================================================
+*/
+
+
+template<typename T, size_t N>
+std::ostream& operator<< (std::ostream& os, const Polynomial<T,N>& poly)
+{
+    int i = N-1;
+    os << poly(i) << "x^" << i;
+    for (int i = N-2; i > 0; --i)
+    {
+        if (poly(i) > 0)
+            os << " + " << std::abs(poly(i)) << "x^" << i;
+        else if (poly(i) < 0)
+            os << " - " << std::abs(poly(i)) << "x^" << i;
+        else {}
+    }
+    if (poly(0) > 0) os << " + " << poly(0) << std::endl;
+    if (poly(0) < 0) os << " - " << std::abs(poly(0)) << std::endl;
+    return os;
+}
+
+template<typename T1, size_t M, 
+        typename T2, size_t N>
+typename std::enable_if<std::is_floating_point<T1>::value || std::is_floating_point<T2>::value, Polynomial<double,std::max(M,N)>>::type
+operator+ (Polynomial<T1,M> first, Polynomial<T2,N> second)
+{
+    auto res_data = std::array<double,std::max(M,N)>();
+    auto res = Polynomial<double,std::max(M,N)> (res_data);
+    for (size_t i = 0; i<std::max(M,N); ++i)
+    {
+        if (i >= M) res(i) = second(i);
+        else
+        if (i >= N) res(i) = first(i);
+        else
+        {
+            res(i) = first(i) + second(i);
+        }
+    }
+    return res;
+}
+
+template<typename T1, size_t M, 
+        typename T2, size_t N>
+typename std::enable_if<std::is_integral<T1>::value && std::is_integral<T2>::value, Polynomial<int,std::max(M,N)>>::type
+operator+ (Polynomial<T1,M> first, Polynomial<T2,N> second)
+{
+    auto res_data = std::array<int,std::max(M,N)>();
+    auto res = Polynomial<int,std::max(M,N)> (res_data);
+    for (size_t i = 0; i<std::max(M,N); ++i)
+    {
+        if (i >= M) res(i) = second(i);
+        else
+        if (i >= N) res(i) = first(i);
+        else
+        {
+            res(i) = first(i) + second(i);
+        }
+    }
+    return res;
+}
+
+template<typename T1, size_t M, 
+        typename T2, size_t N>
+typename std::enable_if<std::is_floating_point<T1>::value || std::is_floating_point<T2>::value, Polynomial<double,std::max(M,N)>>::type
+operator- (Polynomial<T1,M> first, Polynomial<T2,N> second)
+{
+    size_t max_size = std::max(M,N);
+    auto res_data = std::array<double,std::max(M,N)>();
+    auto res = Polynomial<double,std::max(M,N)> (res_data);
+    for (size_t i = 0; i<std::max(M,N); ++i)
+    {
+        if (i >= M) res(i) = -second(i);
+        else
+        if (i >= N) res(i) = first(i);
+        else
+        {
+            res(i) = first(i) - second(i);
+        }
+    }
+    return res;
+}
+
+template<typename T1, size_t M, 
+        typename T2, size_t N>
+typename std::enable_if<std::is_integral<T1>::value && std::is_integral<T2>::value, Polynomial<int,std::max(M,N)>>::type
+operator- (Polynomial<T1,M> first, Polynomial<T2,N> second)
+{
+    auto res_data = std::array<int,std::max(M,N)>();
+    auto res = Polynomial<int,std::max(M,N)> (res_data);
+    for (size_t i = 0; i<std::max(M,N); ++i)
+    {
+        if (i >= M) res(i) = -second(i);
+        else
+        if (i >= N) res(i) = first(i);
+        else
+        {
+            res(i) = first(i) - second(i);
+        }
+    }
+    return res;
+}
