81e537121b4646a4fd837138c63ab007bc7fc3ba
Marcel Koch <marcel.koch@uni-muenster.de>
2020-06-25 11:56:15 +0200

    force conversion to `Vector`

--- blatt08/test.cpp
+++ blatt08/test.cpp
@@ -7,15 +7,20 @@ int main(){
     Vector b{2, 3, 4};
     Vector c{3, 4, 5};
 
-    auto t1 = a + b + c;
-    auto t2 = a - a;
+    Vector t1 = a + b + c;
+    Vector t2 = a - a;
+    std::cout << t1 << std::endl;
+    std::cout << t2 << std::endl;
 
     double alpha = 3.14;
     Vector d{4, 5, 6};
     Vector e{5, 6, 7};
     Vector f{6, 7, 8};
-    auto t3 = alpha * (d + e) + f;
-    auto t4 = (e + d) / 2.;
+
+    Vector t3 = alpha * (d + e) + f;
+    Vector t4 = (e + d) / 2.;
+    std::cout << t3 << std::endl;
+    std::cout << t4 << std::endl;
 
     Vector g{7, 8, 9};
     Vector h{8, 9, 10};
@@ -23,6 +28,8 @@ int main(){
 
     double t5 = g * h;
     Vector t6 = (g * h) * i;
+    std::cout << t5 << std::endl;
+    std::cout << t6 << std::endl;
 
     Vector j{10, 11, 12};
     Vector k{11, 12, 13};
@@ -32,5 +39,6 @@ int main(){
                      {-1, 2, -1},
                      {0, -1, 2}}};
 
-    auto t7 = j + M * (k - l);
+    Vector t7 = j + M * (k - l);
+    std::cout << t7 << std::endl;
 }
